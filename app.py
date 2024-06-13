14fb20c4a97067d04143e259d36bab3d630dbf09
tn-junior <juju2004.tary@gmail.com>
2024-06-13 13:50:01 -0300

    adding attachments form to certificates

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
-from flask import Flask, render_template, request, redirect, session, flash
+from flask import Flask, render_template, request, redirect, session, flash, url_for
 from werkzeug.utils import secure_filename
 import os
 from forms import UploadForm
 
 class Jogo:
     def __init__(self, nome, categoria, console):
-        self.nome=nome
-        self.categoria=categoria
-        self.console=console
+        self.nome = nome
+        self.categoria = categoria
+        self.console = console
 
 jogo1 = Jogo('Tetris', 'Puzzle', 'Atari')
 jogo2 = Jogo('God of War', 'Hack n Slash', 'PS2')
@@ -22,29 +22,16 @@ app.config['UPLOAD_FOLDER'] = 'uploads'
 def index():
     return render_template('lista.html', titulo='Jogos', jogos=lista)
 
-@app.route('/novo')
-def novo():
-    return render_template('novo.html', titulo='Novo Jogo')
-
-@app.route('/criar', methods=['POST',])
-def criar():
-    nome = request.form['nome']
-    categoria = request.form['categoria']
-    console = request.form['console']
-    jogo = Jogo(nome, categoria, console)
-    lista.append(jogo)
-    return redirect('/')
-
 @app.route('/login')
 def login():
     return render_template('login.html')
 
 @app.route('/autenticar', methods=['POST',])
 def autenticar():
-    if 'alohomora' == request.form['senha']:
+    if 'ggie' == request.form['senha']:
         session['usuario_logado'] = request.form['usuario']
         flash(session['usuario_logado'] + ' logado com sucesso!')
-        return redirect('/')
+        return redirect('/upload')
     else:
         flash('Usuário não logado.')
         return redirect('/login')
