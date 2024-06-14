d9fde154651d9591f9e029da52febbbc481114c6
TaryNascimento <tary.junior47@gmail.com>
2024-06-13 21:40:13 -0300

    updating

--- app.py
+++ app.py
@@ -7,6 +7,39 @@ app = Flask(__name__)
 app.secret_key = 'alura'
 app.config['UPLOAD_FOLDER'] = 'uploads'
 
+def calcular_pontos(certificado):
+    pontos = 0
+    nome = certificado['nome']
+    horas = certificado['horas']
+
+    if 'seminários' in nome or 'congressos' in nome or 'oficinas' in nome:
+        pontos = (horas // 20) * 2
+    elif 'atualização' in nome:
+        if horas >= 40:
+            pontos = 5
+    elif 'aperfeiçoamento' in nome:
+        if horas >= 180:
+            pontos = 10
+    elif 'graduação' in nome or 'especialização' in nome:
+        if horas == 360:
+            pontos = 20
+    elif 'Mestrado' in nome or 'Doutorado' in nome or 'Pós-doutorado' in nome:
+        pontos = 30
+    elif 'Instrutoria' in nome or 'Coordenação' in nome:
+        pontos = (horas // 8) * 2
+        if pontos > 10:
+            pontos = 10
+    elif 'grupos' in nome or 'equipes' in nome or 'comissões' in nome or 'projetos especiais' in nome:
+        pontos = 5
+        if pontos > 10:
+            pontos = 10
+    elif 'cargos comissionados' in nome or 'funções gratificadas' in nome:
+        pontos = (horas // 12) * 10  # assumindo que 'horas' está em meses
+        if pontos > 15:
+            pontos = 15
+
+    return pontos
+
 @app.route('/')
 def index():
     return render_template('home.html', titulo='Bem-vindo ao Certification')
