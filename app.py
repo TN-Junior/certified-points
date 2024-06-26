278b10a40633771b96d78a3bb07be303bfe64d03
Tary Junior <111458237+tn-junior@users.noreply.github.com>
2024-06-25 23:06:28 -0300

    Up app.py

--- app.py
+++ app.py
@@ -7,17 +7,27 @@ from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
 from werkzeug.security import generate_password_hash, check_password_hash
 import pymysql
+from wtforms import StringField, IntegerField, FileField, SubmitField, SelectField
+from flask_wtf import FlaskForm
+from wtforms.validators import DataRequired, Optional
+from functools import wraps
+from flask import abort
+from wtforms.validators import DataRequired, Email
+from flask_migrate import Migrate
+
 
 app = Flask(__name__)
 load_dotenv()
 app.secret_key = os.getenv('SECRET_KEY')
 app.config['UPLOAD_FOLDER'] = os.getenv('UPLOAD_FOLDER')
 
+
 # Configuração da conexão com o banco de dados MySQL
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+
 
 # Modelos de dados
 class Certificado(db.Model):
@@ -35,9 +45,73 @@ class Usuario(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     pontuacao = db.Column(db.Integer, default=0)
     senha = db.Column(db.String(120), nullable=False)
+    role = db.Column(db.String(20), default='user')  # Correção aqui
 
     def __repr__(self):
         return f'<Usuario {self.nome}>'
+
+
+    def __repr__(self):
+        return f'<Usuario {self.nome}>'
+    
+class UploadForm(FlaskForm):
+    qualificacao = StringField('Qualificação', validators=[DataRequired()])
+    periodo = StringField('Período', validators=[Optional()])
+    horas = IntegerField('Horas', validators=[DataRequired()])
+    quantidade = IntegerField('Quantidade', validators=[Optional()])
+    pontos = IntegerField('Pontos', validators=[Optional()])
+    ano_conclusao = IntegerField('Ano de Conclusão', validators=[Optional()])
+    ato_normativo = StringField('Ato Normativo', validators=[Optional()])
+    tempo = IntegerField('Tempo (anos)', validators=[Optional()])
+    certificate = FileField('Certificado', validators=[DataRequired()])
+    submit = SubmitField('Enviar')
+
+
+def requires_admin(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        usuario = Usuario.query.filter_by(matricula=session.get('usuario_logado')).first()
+        if usuario and usuario.role == 'admin':
+            return f(*args, **kwargs)
+        else:
+            flash('Acesso negado. Área restrita a administradores.')
+            return redirect(url_for('index'))
+    return decorated_function
+
+def calcular_pontos(certificado_data):
+    qualificacao = certificado_data['qualificacao']
+    horas = certificado_data['horas']
+    pontos = 0
+    
+    if qualificacao == 'Cursos, seminários, congressos e oficinas realizados, promovidos, articulados ou admitidos pelo Município do Recife.':
+        pontos = (horas // 20) * 2
+    elif qualificacao == 'Cursos de atualização realizados, promovidos, articulados ou admitidos pelo Município do Recife.':
+        if horas >= 40:
+            pontos = 5
+    elif qualificacao == 'Cursos de aperfeiçoamento realizados, promovidos, articulados ou admitidos pelo Município do Recife.':
+        if horas >= 180:
+            pontos = 10
+    elif qualificacao == 'Cursos de graduação e especialização realizados em instituição pública ou privada, reconhecida pelo MEC.':
+        if horas >= 360:
+            pontos = 20
+    elif qualificacao == 'Mestrado, doutorado e pós-doutorado realizados em instituição pública ou privada, reconhecida pelo MEC.':
+        pontos = 30
+    elif qualificacao == 'Instrutoria ou Coordenação de cursos promovidos pelo Município do Recife.':
+        pontos = (horas // 8) * 2
+        if pontos > 10:
+            pontos = 10
+    elif qualificacao == 'Participação em grupos, equipes, comissões e projetos especiais, no âmbito do Município do Recife, formalizados por ato oficial.':
+        pontos = 5
+        if pontos > 10:
+            pontos = 10
+    elif qualificacao == 'Exercício de cargos comissionados e funções gratificadas, ocupados, exclusivamente, no âmbito do Poder Executivo Municipal.':
+        pontos = (horas // 12) * 10  # Assumindo que 'tempo' foi fornecido em meses, 12 meses = 1 ano
+        if pontos > 15:
+            pontos = 15
+    
+    return pontos
+
+
 
 @app.route('/')
 def index():
@@ -55,11 +129,17 @@ def autenticar():
 
     if usuario_db and check_password_hash(usuario_db.senha, senha):
         session['usuario_logado'] = usuario
-        flash(usuario + ' logado com sucesso!')
-        return redirect('/upload')
+        flash(f'{usuario} logado com sucesso!')
+        # Verifica o role do usuário e redireciona conforme necessário
+        if usuario_db.role == 'admin':
+            return redirect(url_for('certificados'))
+        else:
+            return redirect(url_for('upload'))
     else:
         flash('Usuário ou senha inválidos.')
         return redirect('/login')
+
+
 
 @app.route('/logout')
 def logout():
@@ -71,15 +151,22 @@ def logout():
 def upload():
     form = UploadForm()
     if form.validate_on_submit():
-        curso = form.curso.data
-        carga_horaria = form.carga_horaria.data
-        pontos = calcular_pontos({'nome': curso, 'horas': carga_horaria})
+        certificado_data = {
+            'qualificacao': form.qualificacao.data,
+            'horas': form.horas.data,
+        }
+        pontos = calcular_pontos(certificado_data)
         file = form.certificate.data
         filename = secure_filename(file.filename)
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        
+        # Verificar e criar o diretório de uploads, se necessário
+        upload_folder = app.config['UPLOAD_FOLDER']
+        if not os.path.exists(upload_folder):
+            os.makedirs(upload_folder)
+
+        file.save(os.path.join(upload_folder, filename))
         session['certificado'] = {
-            'curso': curso,
-            'carga_horaria': carga_horaria,
+            'qualificacao': form.qualificacao.data,
             'pontos': pontos,
             'filename': filename
         }
@@ -88,6 +175,7 @@ def upload():
     return render_template('upload.html', form=form)
 
 @app.route('/certificados')
+@requires_admin
 def certificados():
     certificado = session.get('certificado')
     if not certificado:
@@ -115,29 +203,33 @@ def signup():
 
 @app.route('/cadastrar', methods=['POST'])
 def cadastrar():
-    matricula = request.form['matricula']
-    nome = request.form['nome']
-    email = request.form['email']
-    senha = request.form['senha']
-    hashed_senha = generate_password_hash(senha, method='scrypt')
-
-    novo_usuario = Usuario(matricula=matricula, nome=nome, email=email, senha=hashed_senha)
-
     try:
+        matricula = request.form['matricula']
+        nome = request.form['nome']
+        email = request.form['email']
+        senha = request.form['senha']
+        role = request.form['role']
+        hashed_senha = generate_password_hash(senha, method='scrypt')
+
+        novo_usuario = Usuario(matricula=matricula, nome=nome, email=email, senha=hashed_senha, role=role)
         db.session.add(novo_usuario)
         db.session.commit()
         flash(f'Usuário {nome} cadastrado com sucesso!')
         return redirect('/login')
     except Exception as e:
+        print(e)
         db.session.rollback()
         flash(f'Erro ao cadastrar usuário: {str(e)}')
         return redirect('/signup')
 
+
+# Lista todos os usuários (Read)
 @app.route('/usuarios')
 def listar_usuarios():
     usuarios = Usuario.query.all()
     return render_template('usuarios.html', usuarios=usuarios)
 
+# Atualiza um usuário (Update)
 @app.route('/editar_usuario/<int:id>', methods=['GET', 'POST'])
 def editar_usuario(id):
     usuario = Usuario.query.get(id)
@@ -156,6 +248,7 @@ def editar_usuario(id):
             flash(f'Erro ao atualizar usuário: {str(e)}')
     return render_template('editar_usuario.html', usuario=usuario)
 
+# Deleta um usuário (Delete)
 @app.route('/deletar_usuario/<int:id>', methods=['POST'])
 def deletar_usuario(id):
     usuario = Usuario.query.get(id)
@@ -181,6 +274,8 @@ def cursos():
         {"nome": "Exercício de cargos comissionados e funções gratificadas, ocupados, exclusivamente, no âmbito do Poder Executivo Municipal.", "pontos": 3}
     ]
     return render_template('cursos.html', cursos=cursos_list)
+
+
 
 if __name__ == '__main__':
     with app.app_context():
