79220d8d53b309dfeaf150e0f8e1c48ecb666301
Tary Junior <111458237+tn-junior@users.noreply.github.com>
2024-07-03 15:01:25 -0300

    Update app.py

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-# Defina a timezone usando pytz
+# Configurar a timezone e o scheduler
 timezone = pytz.timezone('America/Recife')
 scheduler = BackgroundScheduler(timezone=timezone)
 
@@ -59,8 +59,9 @@ def limpar_certificados():
     print("Certificados antigos foram limpos.")
 
 # Agendar a tarefa para rodar a cada 24 horas
-scheduler.add_job(limpar_certificados, 'interval', hours=24)
-scheduler.start()
+def iniciar_scheduler():
+    scheduler.add_job(limpar_certificados, 'interval', hours=24)
+    scheduler.start()
 
 class Usuario(db.Model):
     __tablename__ = 'usuarios'
@@ -320,4 +321,5 @@ def deletar_certificado(certificado_id):
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
+        iniciar_scheduler()
     app.run(host='0.0.0.0', port=5000, debug=True)
